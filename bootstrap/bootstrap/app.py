fdc2271290e237a60d28de980f3b0445d6856405
rongfengliang <1141591465@qq.com>
2024-03-16 22:30:34 +0800

    add more

--- bootstrap/bootstrap/app.py
+++ bootstrap/bootstrap/app.py
@@ -3,6 +3,6 @@ from sharespec import MyPlugin
 
 pm = pluggy.PluginManager("mydemoapp")
 pm.add_hookspecs(MyPlugin)
-pm.load_setuptools_entrypoints("mydemoapp")
+pm.load_setuptools_entrypoints("mydemoapptest")
 result = pm.get_plugin("plugina").add(1, 2)
 print(result)
