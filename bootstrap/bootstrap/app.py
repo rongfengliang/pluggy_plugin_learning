89f2d04ca9cc5f25067b19e9aa281b02ab2442aa
rongfengliang <1141591465@qq.com>
2024-03-17 00:12:02 +0800

    add fire cli support

--- bootstrap/bootstrap/app.py
+++ bootstrap/bootstrap/app.py
@@ -1,8 +1,18 @@
+import fire
 import  pluggy
 from sharespec import MyPlugin
+class App:
+    def __init__(self):
+        self.__pm = pluggy.PluginManager("mydemoapp")
+        self.__pm.add_hookspecs(MyPlugin)
+        self.__pm.load_setuptools_entrypoints("mydemoapptest")
 
-pm = pluggy.PluginManager("mydemoapp")
-pm.add_hookspecs(MyPlugin)
-pm.load_setuptools_entrypoints("mydemoapptest")
-result = pm.get_plugin("plugina").add(1, 2)
-print(result)
+    def run(self,plugin_name:str = "plugina"):
+        result = self.__pm.get_plugin(plugin_name).add(1, 2)
+        print(result)
+
+def main():
+    fire.Fire(App)
+
+if __name__ == "__main__":
+    main()
